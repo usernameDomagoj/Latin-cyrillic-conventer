68e33efec47b92d427bc5525cf04ebe629ff18ca
domagoj333 <domagoj_kostic@net.hr>
2020-11-12 13:34:39 +0100

    added vice versa, code cleaned

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const cyr = document.querySelector('.cyr');
 const alphabetButton = document.querySelector('.alphabet');
 const clearButton = document.querySelector('.clear');
 
-const alphabetObj = {'DŽ':'Џ', 'NJ':'Њ', 'LJ':'Љ', 'Dž':'Џ', 'Lj':'Љ', 'Nj':'Њ', 'a':'a', 'b':'б', 'c':'ц',
-			'č':'ч', 'ć':'ћ', 'd':'д', 'dž':'џ', 'đ':'ђ', 'e':'е', 'f':'ф', 'g':'г', 'h':'х', 'i':'и',
-			'j':'ј', 'k':'к', 'l':'л', 'lj':'љ', 'm':'м', 'n':'н', 'nj':'њ', 'o':'о', 'p':'п', 'r':'р',
-			's':'с', 'š':'ш', 't':'т', 'u':'у', 'v':'в', 'z':'з', 'ž':'ж', 'A':'А', 'B':'Б', 'C':'Ц',
-			'Č':'Ч', 'Ć':'Ћ', 'D':'Д', 'Đ':'Ђ', 'E':'Е', 'F':'Ф', 'G':'Г', 'H':'Х', 'I':'И', 'J':'Ј',
-			'K':'К', 'L':'Л', 'M':'М', 'N':'Н', 'O':'О', 'P':'П', 'R':'Р', 'S':'С', 'Š':'Ш', 'T':'Т',
-			'U':'У', 'V':'В', 'Z':'З', 'Ž':'Ж'
+const alphabetObj = { DŽ:'Џ', NJ:'Њ', LJ:'Љ', Dž:'Џ', Lj:'Љ', Nj:'Њ', a:'a', b:'б', c:'ц',
+			č:'ч', ć:'ћ', d:'д', dž:'џ', đ:'ђ', e:'е', f:'ф', g:'г', h:'х', i:'и', j:'ј',
+			k:'к', l:'л', lj:'љ', m:'м', n:'н', nj:'њ', o:'о', p:'п', r:'р', s:'с', š:'ш',
+			t:'т', u:'у', v:'в', z:'з', ž:'ж', A:'А', B:'Б', C:'Ц', Č:'Ч', Ć:'Ћ', D:'Д',
+			Đ:'Ђ', E:'Е', F:'Ф', G:'Г', H:'Х', I:'И', J:'Ј', K:'К', L:'Л', M:'М', N:'Н',
+			O:'О', P:'П', R:'Р', S:'С', Š:'Ш', T:'Т', U:'У', V:'В', Z:'З', Ž:'Ж'
 };
 
-//functions
+
 const alphabet = (e) => {
 	let pattern = 'a, b, c, č, ć, d, dž, đ, e, f, g, h, i, j, k, l, lj, m, n, nj, o, p, r, s, š, t, u, v, z, ž';
+
 	e.shiftKey ? glag.value = pattern.toUpperCase() : glag.value = pattern;
+	replaceLatinCombined();
 };
 
 const replaceLatin = (glagVal, alphabetObj) => {
@@ -23,16 +24,34 @@ const replaceLatin = (glagVal, alphabetObj) => {
 
     return glagVal.replace(re, matched => {
         return alphabetObj[matched];
-    });
+	});
 };
 
-const combinedReplace = () => {
+const replaceCyrilic = (cyrVal, alphabetObje) => {
+	let cyrValArr = cyrVal.split('');
+	let resultCombined = '';
+
+	cyrValArr.forEach(e => {
+		testing = Object.keys(alphabetObje).find(key => alphabetObje[key] === e);
+		testing !== undefined ? resultCombined += testing : resultCombined += e;
+	});
+	glag.value = resultCombined;
+};
+
+const replaceLatinCombined = () => {
 	let glagVal = glag.value;
 	cyr.value = replaceLatin(glagVal, alphabetObj);
 };
 
-//eventListeners
-glag.addEventListener('keyup', combinedReplace);
+const replaceCyrCombined = () => {
+	let cyrVal = cyr.value;
+	glagVal = replaceCyrilic(cyrVal, alphabetObj);
+};
+
+
+glag.addEventListener('keyup', replaceLatinCombined);
+
+cyr.addEventListener('keyup', replaceCyrCombined);
 
 clearButton.addEventListener('click', () => {
 	glag.value = cyr.value = null;
@@ -40,30 +59,4 @@ clearButton.addEventListener('click', () => {
 
 alphabetButton.addEventListener('click', (e) => {
 	alphabet(e);
-	combinedReplace();
 });
-
-
-///testing...
-function arabicToRoman(number){
-	let roman = "";
-	const romanNumList = {M:1000,CM:900, D:500,CD:400, C:100, XC:90,L:50, XV: 40, X:10, IX:9, V:5, IV:4, I:1};
-	let a;
-	if(number < 1 || number > 3999)
-	  return "Enter a number between 1 and 3999";
-	else{
-	  for(let key in romanNumList){
-		  a = Math.floor(number / romanNumList[key]);
-		  if(a >= 0){
-			  for(let i = 0; i < a; i++){
-				roman += key;
-			  }
-			}
-		  number = number % romanNumList[key];
-	  }
-	}
-  
-	return roman;
-  }
-
-  console.log(arabicToRoman(1990));
